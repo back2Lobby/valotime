ed612ea972bb1a6670d740daa77695433431a412
8ack2Lobby <tayyabisking1101@gmail.com>
2022-11-12 20:31:37 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// debugger;
 //redirect to custom domain if its not using it
-if(location.host.search("valotime.live") === -1){
+// if(location.host.search("valotime.live") === -1){
   location.href = "https://valotime.live";
-}
+// }
 
 createApp(App).mount('#app')
