862e7c0335a410af049fdd3323c1381f5cacc853
8ack2Lobby <tayyabisking1101@gmail.com>
2022-11-12 20:19:57 +0500

    Auto Redirect to Custom Domain

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+//redirect to custom domain if its not using it
+if(location.host.search("valotime.live") === -1){
+  location.href = "https://valotime.live";
+}
+
 createApp(App).mount('#app')
