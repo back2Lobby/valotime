69fff8fedcd982d19a4ee599e0e10308303062ec
8ack2Lobby <tayyabisking1101@gmail.com>
2023-01-10 18:32:44 +0500

    Removed the redirection logic

--- src/main.js
+++ src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
-// debugger;
-//redirect to custom domain if its not using it
-if(location.host.includes("valotime.live") === false){
-  location.href = "https://valotime.live";
-}
-
 createApp(App).mount('#app')
